b64ddc714cc95044e445d697415e232178c7acb1
Danyal Ali <alidanyal290@gmail.com>
2025-12-11 17:42:20 +0530

    Add files via upload

---
+++ ConnectSQL.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+public class ConnectSQL {
+    Connection con;
+    Statement stm;
+    ConnectSQL(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ShriNik","root","********");
+            stm = con.createStatement();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+    public static void main(String[] args) {
+        new ConnectSQL();
+    }
+}
